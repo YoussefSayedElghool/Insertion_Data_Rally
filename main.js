64b7b5cea17220604af672e542267c0cd9e8d731
YS_My <www.yyalgol@gmail.com>
2023-07-09 08:17:54 +0300

    fixed the bug of the doupdowen list by use datalist html element

--- main.js
+++ main.js
@@ -1,56 +1,9 @@
 
-ActiveElement = null
-
-done = false
-function Dropdown(e) {
-  if (ActiveElement != null) {
-    
-    ActiveElement.classList.remove("show");
-    
-  }else{
-    e.target.parentElement.children[1].classList.add("show");
-  }
-  ActiveElement =  e.target.parentElement.children[1];
-  done = false
-  console.log(e.target)
-  
-}
-
-function DropUp() {
-  if(done){
-
-    ActiveElement.classList.remove("show");
-    console.log(ActiveElement)
-    ActiveElement = null
-    done = false
-  }
-
-
-}
-
 function Chooes(e) {
   e.target.parentElement.parentElement.children[0].value =
     e.target.innerText;
   console.log(e.target.parentElement.children[0]);
-  done = true
-  DropUp();
 }
-
-function filter(e) {
-  var input = e.target;
-  var filter = input.value.toUpperCase();
-  var div = e.target.parentElement.children[1];
-  var a = div.children;
-
-  for (var i = 0; i < a.length; i++) {
-    txtValue = a[i].textContent || a[i].innerText;
-
-    if (txtValue.toUpperCase().indexOf(filter) > -1)
-      a[i].style.display = "";
-    else a[i].style.display = "none";
-  }
-}
-
 
 
 function increasInfo(e) {
